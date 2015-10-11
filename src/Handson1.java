3c7803cd1562106b0ea1dcc34c12bc3e56305894
geet22ks <gs82@st-andrews.ac.uk>
2015-10-11 16:52:49 +0100

    Exercise5

---
+++ src/Handson1.java
@@ -0,0 +1,17 @@
+
+public class Handson1 {
+public int multiply (int a,int b)
+{
+	int sum;
+	sum=a*b;
+	return(sum);
+	}
+public double multiply (double a,double b)
+{
+	double sum;
+	sum=a*b;
+	return(sum);
+	}
+}
+
+
---
+++ src/Handson5.java
@@ -0,0 +1,30 @@
+
+public class Handson5 extends Handson1 {
+	
+public int multiply(int a,int b){
+	int sum;
+	sum=a+b;
+	return(sum);
+	
+}
+public double multiply(double a,double b){
+	double sum;
+	sum=a+b;
+	return(sum);
+	}
+
+public static void main(String[] args) {
+Handson1 hand1=new Handson1();
+int overes=hand1.multiply(4,5);
+double overes1=hand1.multiply(4.5,5.6);
+Handson5 hand5=new Handson5();
+int res=hand5.multiply(4,5);
+double res1=hand5.multiply(4.5,5.6);
+System.out.println("Overridden method values"+overes+"  "+overes1);
+System.out.println("\n Normal method values"+res+"  "+res1);
+
+// TODO Auto-generated method stub
+
+	}
+
+}
